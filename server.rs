65c95ea3ff5ec384380bd0dc02a3d7b992883e9d
Darko Donnie <donni3thedarko@gmail.com>
2020-07-10 01:50:26 +0000

    Initial commit

---
+++ server.rs
@@ -0,0 +1,15 @@
+let addr = ([127, 0, 0, 1], 8080).into();
+let builder = Server::bind(&addr);
+let server = builder.serve(|| {
+  service_fn_ok(|_| {
+
+    Response::new(Body::from("Almost microservice..."))
+
+  })
+})
+
+let server = server.map_err(drop);
+hyper::rt::run(server);
+use hyper::{Body, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
